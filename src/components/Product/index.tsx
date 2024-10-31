5267204d694d249435fa46ecc2063a78de4dc834
Vinicius Santos <vinisantoosk@gmail.com>
2024-10-31 09:34:36 -0300

    feat: renderizando diferentes tags

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,18 +1,34 @@
 import Tag from '../Tag'
-import { Card, Descricao, Titulo } from './styles'
+import { Card, Descricao, Infos, Titulo } from './styles'
 
-const Product = () => (
+type Props = {
+  title: string
+  category: string
+  system: string
+  description: string
+  infos: string[]
+  image: string
+}
+
+const Product = ({
+  title,
+  category,
+  system,
+  description,
+  infos,
+  image
+}: Props) => (
   <Card>
-    <img src="//placehold.co/222x250" />
-    <Titulo>Novo jogo</Titulo>
-    <Tag>Categoria</Tag>
-    <Tag>Windows</Tag>
-    <Descricao>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat
-      voluptate eveniet totam ipsa mollitia maiores, minus ad laborum nam
-      suscipit earum itaque temporibus dicta aspernatur ipsam nobis optio
-      facilis qui!
-    </Descricao>
+    <img src={image} alt={title} />
+    <Infos>
+      {infos.map((info) => (
+        <Tag key={info}>{info}</Tag>
+      ))}
+    </Infos>
+    <Titulo>{title}</Titulo>
+    <Tag>{category}</Tag>
+    <Tag>{system}</Tag>
+    <Descricao>{description}</Descricao>
   </Card>
 )
 
--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -2,20 +2,48 @@ import { title } from 'process'
 import Product from '../Product'
 import { Container, List } from './styles'
 
-type Props = {
+export type Props = {
   title: string
   background: 'gray' | 'black'
 }
 
 const ProductList = ({ background, title }: Props) => (
-  <Container>
+  <Container background={background}>
     <div className="container">
       <h2>{title}</h2>
       <List>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        <Product
+          title="Jgo"
+          system="windows"
+          category="Ação"
+          description=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem maxime delectus, aliquid reiciendis cupiditate, repudiandae rem omnis magni voluptate excepturi eligendi, in pariatur ullam soluta adipisci natus rerum? Quo, expedita."
+          image="//placehold.co/222x250"
+          infos={['-10%', 'R$ 150']}
+        />
+        <Product
+          title="Jgo"
+          system="windows"
+          category="Ação"
+          description=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem maxime delectus, aliquid reiciendis cupiditate, repudiandae rem omnis magni voluptate excepturi eligendi, in pariatur ullam soluta adipisci natus rerum? Quo, expedita."
+          image="//placehold.co/222x250"
+          infos={['-10%', 'R$ 150']}
+        />
+        <Product
+          title="Jgo"
+          system="windows"
+          category="Ação"
+          description=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem maxime delectus, aliquid reiciendis cupiditate, repudiandae rem omnis magni voluptate excepturi eligendi, in pariatur ullam soluta adipisci natus rerum? Quo, expedita."
+          image="//placehold.co/222x250"
+          infos={['-10%', 'R$ 150']}
+        />
+        <Product
+          title="Jgo"
+          system="windows"
+          category="Ação"
+          description=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem maxime delectus, aliquid reiciendis cupiditate, repudiandae rem omnis magni voluptate excepturi eligendi, in pariatur ullam soluta adipisci natus rerum? Quo, expedita."
+          image="//placehold.co/222x250"
+          infos={['-10%', 'R$ 150']}
+        />
       </List>
     </div>
   </Container>
